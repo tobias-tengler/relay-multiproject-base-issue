139ff0839d372c9500e48cf12f90c086a226b649
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2023-10-11 17:13:06 +0200

    Error reproduction with JS

---
+++ base/__generated__/baseFragment.graphql.js
@@ -0,0 +1,31 @@
+/**
+ * @generated SignedSource<<5a09dab4f9163739c91bbf142ac88ed3>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+var node = {
+  "argumentDefinitions": [],
+  "kind": "Fragment",
+  "metadata": null,
+  "name": "baseFragment",
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "name",
+      "storageKey": null
+    }
+  ],
+  "type": "User",
+  "abstractKey": null
+};
+
+node.hash = "cf23b1016277de917a29d8993ed6658a";
+
+export default node;
